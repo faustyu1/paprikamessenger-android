53e9e57547c566c23c80471c6c61670d400d29a0
Daniil Shashimov <faustyu@vk.com>
2026-03-26 01:56:35 +0400

    fix: remove search history duplicates by making query the primary key

--- app/src/main/java/ru/faustyu/paprika/data/db/AppDatabase.kt
+++ app/src/main/java/ru/faustyu/paprika/data/db/AppDatabase.kt
@@ -3,7 +3,7 @@ package ru.faustyu.paprika.data.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [MessageEntity::class, UserEntity::class, SearchHistoryEntity::class, StoryEntity::class], version = 5, exportSchema = false)
+@Database(entities = [MessageEntity::class, UserEntity::class, SearchHistoryEntity::class, StoryEntity::class], version = 6, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun messageDao(): MessageDao
     abstract fun userDao(): UserDao
